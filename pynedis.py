fbd3eb2b2e28359af6f665c523eb320976167224
Baldurrr <77190420+Baldurrr@users.noreply.github.com>
2021-11-01 17:49:55 +0100

    Update pynedis.py

--- pynedis.py
+++ pynedis.py
@@ -10,6 +10,7 @@ import time
 from datetime import datetime,timedelta
 import itertools
 
+
 while True:
 
     print("\n")
@@ -82,7 +83,7 @@ while True:
             start_date=SEARCH_DATE.strftime("%Y-%m-%d")
             end_date=DATE_TODAY.strftime("%Y-%m-%d")
             
-            print("["+datetime.now().strftime("%d/%m/%Y - %H:%M:%S")+"] | Start date: "+str(start_date)+"- End date: "+str(end_date))
+            print("["+datetime.now().strftime("%d/%m/%Y - %H:%M:%S")+"] | Start date: "+str(start_date)+" - End date: "+str(end_date))
 
             conn = http.client.HTTPSConnection("enedisgateway.tech")
             payload = {'type': 'consumption_load_curve','usage_point_id': collect_id,'start': start_date,'end': end_date}
@@ -93,15 +94,33 @@ while True:
             decoded_data=data.decode("utf-8")
 
             json_response=json.loads(decoded_data)
-            metric_only=json_response["meter_reading"]["interval_reading"]
+            try:
+                metric_only=json_response["meter_reading"]["interval_reading"]
+
+            except KeyError():
+                time.sleep(20)
+                conn = http.client.HTTPSConnection("enedisgateway.tech")
+                payload = {'type': 'consumption_load_curve','usage_point_id': collect_id,'start': start_date,'end': end_date}
+                headers = {'Authorization': api_token,'Content-Type': "application/json",}
+                conn.request("POST", "/api", json.dumps(payload), headers)
+                res = conn.getresponse()
+                data = res.read()
+                decoded_data=data.decode("utf-8")
+                json_response=json.loads(decoded_data)
+                metric_only=json_response["meter_reading"]["interval_reading"]
 
             conn = mysql.connector.connect(user=sql_user,password=sql_password,host=sql_host,database=sql_db,port=sql_port,auth_plugin='mysql_native_password')
             cur = conn.cursor()
 
             for row_value in metric_only:
                 metric_date=row_value["date"]
+                metric_date=str(metric_date)
+                date_time_obj = datetime.strptime(metric_date, '%Y-%m-%d %H:%M:%S')
+                converted_date= date_time_obj - timedelta(hours=1)
+                print(converted_date)
                 metric_value=row_value["value"]
-                sql_request="INSERT into enedis_metrics (time,metric,conso_watt) VALUES ('"+str(metric_date)+"','consumption_curve',"+str(metric_value)+")"
+
+                sql_request="INSERT into enedis_metrics (time,metric,conso_watt) VALUES ('"+str(converted_date)+"','consumption_curve',"+str(metric_value)+")"
 
                 try:
                     cur.execute(sql_request)
